a2fa052f06a61f291e47b45d11d2aac75c63ba7c
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-06-28 23:50:52 -0300

    projeto Java, começando os trabalhos com rest

--- fontes/Projeto2httpJaxRS/src/com/videoaula/model/Professor.java
+++ fontes/Projeto2httpJaxRS/src/com/videoaula/model/Professor.java
@@ -2,6 +2,9 @@ package com.videoaula.model;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Professor implements Serializable {
 
 	/**
--- fontes/Projeto2httpJaxRS/src/com/videoaula/model/rest/facade/ProfessorFacade.java
+++ fontes/Projeto2httpJaxRS/src/com/videoaula/model/rest/facade/ProfessorFacade.java
@@ -1,9 +1,29 @@
 package com.videoaula.model.rest.facade;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.videoaula.model.Professor;
 
 @Path("/professores")
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+@Consumes(MediaType.APPLICATION_JSON)
 public class ProfessorFacade {
 
-	
+	static List<Professor> listaProfessores = new ArrayList<Professor>();
+
+	static {
+		listaProfessores.add(new Professor(1, "Paulo Franca", "paulo@gmail.com", "(81)98882-4977"));
+	}
+
+	@GET
+	public List<Professor> getProfessores() {
+		return listaProfessores;
+	}
 }
