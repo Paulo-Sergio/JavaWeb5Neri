06ec0149b0cff7d72d50275bfebf39752df02032
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-07-01 14:25:13 -0300

    criando projeto final [Hibernate, CID, RESTFUL, AngularJS...]

---
+++ fontes/Projeto4Final/src/com/videoaula/model/dao/ProfessorDAO.java
@@ -0,0 +1,40 @@
+package com.videoaula.model.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.videoaula.model.model.Professor;
+
+public class ProfessorDAO implements IProfessorDAO {
+
+	@PersistenceContext(unitName = "Projeto4FinalPersistenceUnit")
+	
+	private EntityManager entityManager;
+
+	@Override
+	public Professor salvarProfessor(Professor professor) {
+		this.entityManager.persist(professor);
+		return professor;
+	}
+
+	@Override
+	public void alterarProfessor(Professor professor) {
+		Professor professorMerge = this.entityManager.merge(professor);
+		this.entityManager.persist(professorMerge);
+	}
+
+	@Override
+	public void excluirProfessor(Professor professor) {
+		this.entityManager.remove(professor);
+	}
+
+	@Override
+	public List<Professor> getProfessores() {
+		Query query = this.entityManager.createQuery("from Professor");
+		return query.getResultList();
+	}
+
+}
